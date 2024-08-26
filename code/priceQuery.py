ae861c3f126f3f5332f76a96874248a2ae1fcce9
liguolin <guolin_li@kuaidi100.com>
2024-08-26 14:20:03 +0800

    增加价格查询接口

---
+++ code/priceQuery.py
@@ -0,0 +1,35 @@
+# coding = utf-8
+import hashlib
+import json
+import time
+
+import requests
+
+# 地址解析接口
+def main():
+    url = "https://api.kuaidi100.com/label/order"  # 请求地址
+    key = ""            #客户授权key
+    secret = ""         #秘钥
+    param = {
+        "sendAddr": "深圳南山区金蝶软件园",
+        "receiveAddr": "北京海淀区",
+        "companyName": "jd",
+        "weight": "12"
+    }
+    t = str(int(round(time.time() * 1000)))
+
+    md = hashlib.md5()
+    md.update((str(json.dumps(param)) + t + key + secret).encode())
+    sign = md.hexdigest().upper()
+    payload = {
+        'key': key,
+        't': t,
+        'method':'price',
+        'param': json.dumps(param),
+        'sign': sign
+    }
+    return requests.post(url = url, data = payload).content.decode("utf-8")
+
+
+if __name__ == '__main__':
+    print(main())
